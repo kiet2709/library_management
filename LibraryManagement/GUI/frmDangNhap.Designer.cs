fc0e13b4cb720196fe1df01b3a816fdb7ef7b905
Tuan Kiet <tuankietlenguyen2709@gmail.com>
2022-10-25 21:42:07 +0700

    Complete frmDangNhap

---
+++ LibraryManagement/GUI/frmDangNhap.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace LibraryManagement
+{
+    partial class frmDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlAnhDangNhap = new System.Windows.Forms.Panel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pnlDangNhap = new System.Windows.Forms.Panel();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.panel23 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.txtTenDangNhap = new System.Windows.Forms.TextBox();
+            this.panel22 = new System.Windows.Forms.Panel();
+            this.panel24 = new System.Windows.Forms.Panel();
+            this.panel25 = new System.Windows.Forms.Panel();
+            this.panel26 = new System.Windows.Forms.Panel();
+            this.btnDangNhapp = new System.Windows.Forms.Button();
+            this.pnlDuongTrongDangNhap = new System.Windows.Forms.Panel();
+            this.pnlAnhDangNhap.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.pnlDangNhap.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.panel23.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlAnhDangNhap
+            // 
+            this.pnlAnhDangNhap.Controls.Add(this.pictureBox4);
+            this.pnlAnhDangNhap.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlAnhDangNhap.Location = new System.Drawing.Point(0, 0);
+            this.pnlAnhDangNhap.Name = "pnlAnhDangNhap";
+            this.pnlAnhDangNhap.Size = new System.Drawing.Size(308, 454);
+            this.pnlAnhDangNhap.TabIndex = 0;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox4.Image = global::LibraryManagement.Properties.Resources.Login;
+            this.pictureBox4.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(308, 454);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 0;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pnlDangNhap
+            // 
+            this.pnlDangNhap.Controls.Add(this.btnDangNhapp);
+            this.pnlDangNhap.Controls.Add(this.panel26);
+            this.pnlDangNhap.Controls.Add(this.panel25);
+            this.pnlDangNhap.Controls.Add(this.panel24);
+            this.pnlDangNhap.Controls.Add(this.panel22);
+            this.pnlDangNhap.Controls.Add(this.pictureBox6);
+            this.pnlDangNhap.Controls.Add(this.pictureBox5);
+            this.pnlDangNhap.Controls.Add(this.panel23);
+            this.pnlDangNhap.Controls.Add(this.txtMatKhau);
+            this.pnlDangNhap.Controls.Add(this.txtTenDangNhap);
+            this.pnlDangNhap.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlDangNhap.Location = new System.Drawing.Point(308, 0);
+            this.pnlDangNhap.Name = "pnlDangNhap";
+            this.pnlDangNhap.Size = new System.Drawing.Size(554, 454);
+            this.pnlDangNhap.TabIndex = 1;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::LibraryManagement.Properties.Resources.Password;
+            this.pictureBox6.Location = new System.Drawing.Point(119, 279);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(40, 51);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::LibraryManagement.Properties.Resources.User;
+            this.pictureBox5.Location = new System.Drawing.Point(119, 158);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(40, 51);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // panel23
+            // 
+            this.panel23.Controls.Add(this.pnlDuongTrongDangNhap);
+            this.panel23.Controls.Add(this.label2);
+            this.panel23.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel23.Location = new System.Drawing.Point(0, 0);
+            this.panel23.Name = "panel23";
+            this.panel23.Size = new System.Drawing.Size(554, 100);
+            this.panel23.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(194, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(232, 51);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Đăng nhập";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMatKhau.Location = new System.Drawing.Point(157, 284);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(275, 45);
+            this.txtMatKhau.TabIndex = 1;
+            // 
+            // txtTenDangNhap
+            // 
+            this.txtTenDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTenDangNhap.Location = new System.Drawing.Point(157, 162);
+            this.txtTenDangNhap.Name = "txtTenDangNhap";
+            this.txtTenDangNhap.Size = new System.Drawing.Size(275, 45);
+            this.txtTenDangNhap.TabIndex = 0;
+            // 
+            // panel22
+            // 
+            this.panel22.Location = new System.Drawing.Point(158, 123);
+            this.panel22.Name = "panel22";
+            this.panel22.Size = new System.Drawing.Size(274, 42);
+            this.panel22.TabIndex = 6;
+            // 
+            // panel24
+            // 
+            this.panel24.Location = new System.Drawing.Point(158, 243);
+            this.panel24.Name = "panel24";
+            this.panel24.Size = new System.Drawing.Size(275, 42);
+            this.panel24.TabIndex = 7;
+            // 
+            // panel25
+            // 
+            this.panel25.Location = new System.Drawing.Point(429, 123);
+            this.panel25.Name = "panel25";
+            this.panel25.Size = new System.Drawing.Size(20, 86);
+            this.panel25.TabIndex = 8;
+            // 
+            // panel26
+            // 
+            this.panel26.Location = new System.Drawing.Point(429, 243);
+            this.panel26.Name = "panel26";
+            this.panel26.Size = new System.Drawing.Size(20, 86);
+            this.panel26.TabIndex = 9;
+            // 
+            // btnDangNhapp
+            // 
+            this.btnDangNhapp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDangNhapp.Location = new System.Drawing.Point(221, 365);
+            this.btnDangNhapp.Name = "btnDangNhapp";
+            this.btnDangNhapp.Size = new System.Drawing.Size(134, 50);
+            this.btnDangNhapp.TabIndex = 10;
+            this.btnDangNhapp.Text = "Đăng nhập";
+            this.btnDangNhapp.UseVisualStyleBackColor = true;
+            // 
+            // pnlDuongTrongDangNhap
+            // 
+            this.pnlDuongTrongDangNhap.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pnlDuongTrongDangNhap.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlDuongTrongDangNhap.Location = new System.Drawing.Point(0, 97);
+            this.pnlDuongTrongDangNhap.Name = "pnlDuongTrongDangNhap";
+            this.pnlDuongTrongDangNhap.Size = new System.Drawing.Size(554, 3);
+            this.pnlDuongTrongDangNhap.TabIndex = 3;
+            // 
+            // frmDangNhap
+            // 
+            this.ClientSize = new System.Drawing.Size(862, 454);
+            this.Controls.Add(this.pnlDangNhap);
+            this.Controls.Add(this.pnlAnhDangNhap);
+            this.Name = "frmDangNhap";
+            this.Text = "Đăng nhập";
+            this.pnlAnhDangNhap.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.pnlDangNhap.ResumeLayout(false);
+            this.pnlDangNhap.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.panel23.ResumeLayout(false);
+            this.panel23.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.Panel panel12;
+        private System.Windows.Forms.Panel panel13;
+        private System.Windows.Forms.Panel panel14;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.Panel panel21;
+        private System.Windows.Forms.Panel panel17;
+        private System.Windows.Forms.Panel panel16;
+        private System.Windows.Forms.Panel panel15;
+        private System.Windows.Forms.Panel panel20;
+        private System.Windows.Forms.Panel panel19;
+        private System.Windows.Forms.Panel panel18;
+        private System.Windows.Forms.Panel pnlAnhDangNhap;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Panel pnlDangNhap;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Panel panel23;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.TextBox txtTenDangNhap;
+        private System.Windows.Forms.Panel panel26;
+        private System.Windows.Forms.Panel panel25;
+        private System.Windows.Forms.Panel panel24;
+        private System.Windows.Forms.Panel panel22;
+        private System.Windows.Forms.Button btnDangNhapp;
+        private System.Windows.Forms.Panel pnlDuongTrongDangNhap;
+    }
+}
+
---
+++ LibraryManagement/GUI/frmDangNhap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LibraryManagement
+{
+    public partial class frmDangNhap : Form
+    {
+        public frmDangNhap()
+        {
+            InitializeComponent();
+            pnlDangNhap.BackColor = Color.FromArgb(255, 255, 255);
+            btnDangNhapp.BackColor = Color.FromArgb(255, 207, 71); 
+            pnlDuongTrongDangNhap.BackColor = Color.FromArgb(255, 207, 71);
+        }
+
+       
+    }
+}
