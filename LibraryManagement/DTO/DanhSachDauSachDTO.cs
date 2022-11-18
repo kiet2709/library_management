90622bfc384781b84b02c5edcdd0eb06fe987310
Tuan Kiet <tuankietlenguyen2709@gmail.com>
2022-11-18 21:54:44 +0700

    update

---
+++ LibraryManagement/DTO/DanhSachDauSachDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace LibraryManagement.DTO
+{
+    public class DanhSachDauSachDTO
+    {
+        private List<DauSachDTO> dauSachDTOs;
+
+        public List<DauSachDTO> DauSachDTOs { get => dauSachDTOs; set => dauSachDTOs = value; }
+        public void add(DataTable dataTable)
+        {
+            foreach (DataRow row in dataTable.Rows)
+            {
+                PhieuMuonDTO temp = new PhieuMuonDTO();
+                temp.addRow(row);
+                
+            }
+        }
+    }
+}
---
+++ LibraryManagement/DTO/DauSachDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LibraryManagement.DTO
+{
+    public class DauSachDTO
+    {
+        private int id;
+        private string name;
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+
+        public void addRow(DataRow row)
+        {
+            id = Convert.ToInt32(row[0]);
+            name = Convert.ToString(row[1]);
+        }
+        
+    }
+}
